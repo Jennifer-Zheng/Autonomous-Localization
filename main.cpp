924ff09435aa14786ff1c1dde1d17e2f52669186
Jennifer Zheng <jenn.zheng@yahoo.com>
2017-04-15 18:43:20 -0500

    updated main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 //Update as needed 
 #include <iostream>
 #include "ros/ros.h"
-#include <geometry_msgs/Twist.h> 
+#include <cstdlib>
+
+//libraries we may need
+/*#include <geometry_msgs/Twist.h>
+#include <tf/tf.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <tf/transform_listener.h>
+#include <actionlib/server/simple_action_server.h>
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseArray.h"
+*/ 
 
 using namespace std;
+//using namespace cv;
 
 int main() {
    return 0;
